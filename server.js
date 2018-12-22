594d3a249dbdaa0d72710646800e459646ecf8d0
Jchn <jchn.wrk@gmail.com>
2018-12-22 11:28:23 -0500

    linked pages

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, function() {
+   console.log("App listening on PORT" + PORT);
+ });
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+   res.sendFile(path.join(__dirname, "index.html"));
+ });
+
+ app.get("/tables", function(req, res) {
+   res.sendFile(path.join(__dirname, "tables.htm"));
+ });
+
+ app.get("/reservation", function(req, res) {
+   res.sendFile(path.join(__dirname, "reservation.htm"));
+ });
